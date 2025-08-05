b1101f904f5267f90e5633bd81361cd5657c248f
andreaslindstrom-ntm <141117488+andreaslindstrom-ntm@users.noreply.github.com>
2025-08-05 08:06:05 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/10yMAgP-Y2BSgNeJ7Tgeqfm2h10CGCYtYGM4wnnscDZE/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyCCxbqzehycUDnfEM-lYMof7rXSQKPBEr8';
+var googleApiKey = 'AIzaSyDJZ6tNBgSh_iVGCvXrJr5BvP2dSE7IIAM';
